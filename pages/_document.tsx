d1e3f84f01e5cfa2a163172317cca46bd7344600
Jonathan Martin <jonathan@ekoha.co>
2021-08-12 09:51:04 +0200

    add mailchimp form

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,6 +10,7 @@ export default class MyDocument extends Document {
             <Html lang="en">
             <Head>
                 <style>{dom.css()}</style>
+                <script id="mcjs" dangerouslySetInnerHTML={{__html: '!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/b10bf3265527eb5ac53cb777f/bc347c85910ae2f36add11af2.js");'}}></script>
             </Head>
             <body>
             <Main/>
